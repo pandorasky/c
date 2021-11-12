655c2b1ebf4a9e07284bda5265c64539f843c7c1
Shivani <rajshivani155@gmail.com>
2021-11-13 01:07:21 +0530

    programming

---
+++ _sum_array_ptr.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int arr[6] = {4,5,6,7,8,9};
+    int sum=0;
+    int *p;
+    p=arr;
+   for(int i=0;i<6;i++){
+      sum = sum + *p;
+      p++;
+   }
+    printf(" %d ",sum);
+}
