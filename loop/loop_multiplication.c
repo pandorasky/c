d8f12800f6d7927c75a186676c85ef62d8078dc5
Shivani <rajshivani155@gmail.com>
2021-12-04 23:16:48 +0530

    loop multiplication

---
+++ loop/loop_multiplication.c
@@ -0,0 +1,12 @@
+
+//he user to input a positive integer. It should then print the multiplication table of that number. 
+#include<stdlib.h>
+#include<stdio.h>
+    void main(){
+        int i,a,b;
+        printf("enter your number");
+        scanf("%d %d",&a,&b);
+        i=a*b;
+        printf("%d",i);
+        
+    }
