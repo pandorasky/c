6bf8b2e0b93e298a74cb972fd7f02321c4f3848d
Shivani <rajshivani155@gmail.com>
2021-12-04 22:34:26 +0530

    loop

---
+++ loop_sum_natural_no.c
@@ -0,0 +1,11 @@
+//Write a program to calculate the sum of first 10 natural number
+#include<stdio.h>
+    void main(){
+        int sum = 0;
+        for ( int i =1; i<=10; i++){
+         sum = sum + i;
+        }
+        printf("%d",sum);
+        
+        
+    }
