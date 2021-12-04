31ab13667a8aef434b18b4e167ed6ec4d7b3b22c
Shivani <rajshivani155@gmail.com>
2021-12-04 22:16:34 +0530

    loop

---
+++ loop/loop_natural_no.c
@@ -0,0 +1,11 @@
+//Write a program in C to display the first 10 natural numbers. Go to the editor
+//Expected Output :
+//1 2 3 4 5 6 7 8 9 10
+
+#include<stdio.h>
+    void main(){
+        int i;
+        for (i=1; i<=10; i++){
+        printf(" %d ",i);
+        }
+    }
