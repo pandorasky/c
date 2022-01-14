da4a6e5cba07d3e4c350e3481ef468c606696073
Shivani <rajshivani155@gmail.com>
2022-01-14 13:38:28 +0530

    count tabs_blank_input_output_files

---
+++ count tabs_blanks_input_output_files.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+    int main(){
+    FILE *fp;
+    char ch;
+    int  nol=0,not= 0,nob=0,noc=0;
+    fp = fopen("arc.c","r");
+    while(1){
+        ch =fgetc(fp);
+        if(ch==EOF)
+        break;
+        noc++;
+        
+        if(ch==' ')
+        nob++;
+        
+        if(ch =='\n')
+        nol++;
+        
+        if(ch=='\t')
+        not++;
+    }
+    fclose(fp);
+    printf("Number of character %d\n",noc);
+    printf("Number of blanks %d\n",nob);
+    printf("Number of tabs %d\n",not);
+    printf("Number of lines%d\n",nol);
+    return 0;
+}
